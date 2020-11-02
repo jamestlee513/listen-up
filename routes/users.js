3ea1818d0566b322d7319074eebd67c247041580
carlos molero <cmolero130@gmail.com>
2020-11-02 17:20:26 -0500

    edit layout.pug and create utils.pug

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const { check, validationResult } = require("express-validator");
 
 const db = require("../db/models"); // check to db
