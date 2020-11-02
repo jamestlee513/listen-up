6fc9d6a418ee0359e7709e78e6b2e9abcbba1e7a
miguelcoria94 <miguelcoria94@gmail.com>
2020-11-02 12:37:12 -0600

    create route for user/register

--- routes/users.js
+++ routes/users.js
@@ -6,8 +6,18 @@ const db = require("../db/models")
 const { loginUser, logoutUser } = require("../auth");
 const { csrfProtection, asyncHandler } = require("./utils");
 
-
 const router = express.Router();
+
+router.get("/user/register", csrfProtection, (req, res) => {
+
+  const user = db.User.build();
+
+  res.render("user-register", {
+    title: "Register",
+    user,
+    csrfToken: req.csrfToken(),
+  })
+})
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
