dc015e5bd18304727f884211ce445e6db42e2968
miguelcoria94 <miguelcoria94@gmail.com>
2020-11-02 12:33:55 -0600

    import modules

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,13 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const bcrypt = require('bcrypt');
+const { check, validationResult } = require("express-validator");
+
+const db = require("../db/models")
+const { loginUser, logoutUser } = require("../auth");
+const { csrfProtection, asyncHandler } = require("./utils");
+
+
+const router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
