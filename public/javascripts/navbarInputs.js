5e0c8a3fed52b9c92c8bde36feeb02e7996eb8d7
miguelcoria94 <miguelcoria94@gmail.com>
2020-11-03 12:28:45 -0600

    fix input color issue

--- public/javascripts/navbarInputs.js
+++ public/javascripts/navbarInputs.js
@@ -1,5 +1,5 @@
 window.addEventListener("DOMContentLoaded", (event) => {
-    const form = document.querySelector(".form_container-form");
+    const form = document.querySelector(".navbar_container-form");
 
     form.addEventListener("input", (event) => {
         event.target.style.color = "black";
