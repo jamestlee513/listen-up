d7c8142c570d8043799648378dc0a468c05eb7eb
James Lee <jamestlee513@gmail.com>
2020-11-05 14:51:17 -0800

    working on formatting ajax

--- public/javascripts/playlist.js
+++ public/javascripts/playlist.js
@@ -1,15 +1,49 @@
-window.addEventListener("DOMContentLoaded", () => {
-    const sidebarContainer = document.querySelector(".sidebar-container__playlists-selections");
-    sidebarContainer.addEventListener("click", async e => {
+window.addEventListener('DOMContentLoaded', () => {
+    const sidebarContainer = document.querySelector('.sidebar-container__playlists-selections');
+    sidebarContainer.addEventListener('click', async e => {
         const selection = e.target;
         const playlistClass = selection.classList[0];
         const index = playlistClass.indexOf('-');
         const id = parseInt(playlistClass.slice(index + 1))
 
         const res = await fetch(`/playlists/${id}`);
-        const data = await res.json();
-        console.log(data);
+        const json = await res.json();
+        const data = json.PlaylistPodcastJoins;
 
-        // console.log(selection);
+        makeActive(selection);
+        populatePodcastContent(data);
+        // console.log(data);
     });
+
+    function makeActive(selection) {
+        sidebarContainer.childNodes.forEach((playlist) => {
+            if (playlist.classList.contains('playlist-active')) {
+                playlist.classList.remove('playlist-active');
+            }
+        })
+        selection.classList.add('playlist-active');
+    }
+
+    function populatePodcastContent(data) {
+        const contentContainer = document.querySelector('.main-container__playlist-content');
+        contentContainer.innerHTML = '';
+        for (let i = 0; i < data.length; i++) {
+            const { description, duration, hostName, id, podcastImage, title } = data[i].Podcast;
+
+            const newPlaylistContent = document.createElement('div');
+            newPlaylistContent.classList.add('podcast__wrapper');
+            newPlaylistContent.innerHTML = `
+            <div class='podcast-img-wrapper'>
+                <img src='../${podcastImage}' alt='podcast image'>
+            </div>
+            <div class='podcast-header'>
+                <h3 class='podcast-title'>${title}</h3>
+                <h4 class='podcast-host'>${hostName}</h4>
+            </div>
+            <p class='podcast-description'>no description for now</p>
+            `
+
+            contentContainer.appendChild(newPlaylistContent);
+        }
+    }
 });
