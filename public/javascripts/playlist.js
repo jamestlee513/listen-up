52950c07b4360b8f972f9ab4e4336f4a8d3d6836
James Lee <jamestlee513@gmail.com>
2020-11-05 15:32:46 -0800

    implemented playlist.js to populate playlist content

--- public/javascripts/playlist.js
+++ public/javascripts/playlist.js
@@ -27,23 +27,55 @@ window.addEventListener('DOMContentLoaded', () => {
     function populatePodcastContent(data) {
         const contentContainer = document.querySelector('.main-container__playlist-content');
         contentContainer.innerHTML = '';
-        for (let i = 0; i < data.length; i++) {
-            const { description, duration, hostName, id, podcastImage, title } = data[i].Podcast;
 
+        // <div class="podcast__wrapper">
+        //     <div class="podcast-img__wrapper">
+        //         <img class="podcast-img" src="" alt="podcast-image"></div>
+        //     <div class="podcast-header">
+        //         <h3 class="podcast-title">Title</h3>
+        //         <h4 class="podcast-host">Host Name</h4>
+        //     </div>
+        //     <p class="podcast-description"></p>
+        // </div>
+
+        for (let i = 0; i < data.length; i++) {
+            const { duration, hostName, id, podcastImage, title } = data[i].Podcast;
+            let { description } = data[i].Podcast;
+            if (description.length > 200) {
+                description = shortenDescription(description);
+            }
             const newPlaylistContent = document.createElement('div');
             newPlaylistContent.classList.add('podcast__wrapper');
             newPlaylistContent.innerHTML = `
-            <div class='podcast-img-wrapper'>
-                <img src='../${podcastImage}' alt='podcast image'>
-            </div>
-            <div class='podcast-header'>
-                <h3 class='podcast-title'>${title}</h3>
-                <h4 class='podcast-host'>${hostName}</h4>
-            </div>
-            <p class='podcast-description'>no description for now</p>
+                <div class="podcast-img__wrapper">
+                    <img class="podcast-img" src="${podcastImage}" alt="podcast-image">
+                </div>
+                <div class="podcast-header">
+                    <h3 class="podcast-title">${title}</h3>
+                    <h4 class="podcast-host">${hostName}</h4>
+                </div>
+                <p class="podcast-description">${description}</p>
             `
 
             contentContainer.appendChild(newPlaylistContent);
         }
+    }
+    function shortenDescription(description) {
+        let words = description.split(' ');
+        let shortened = [];
+        let i = 0;
+        let length = 0;
+        while(length < 200) {
+            shortened.push(words[i]);
+            length += words[i].length;
+            i++;
+        }
+        let shortenedString = shortened.join(' ');
+        if(/([a-zA-Z"']^)/.test(shortenedString[shortenedString.length - 1])) {
+            shortenedString = shortenedString.slice(0, shortenedString.length - 1);
+        }
+        shortenedString += '  ...';
+
+        return shortenedString;
     }
 });
