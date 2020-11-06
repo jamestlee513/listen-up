78acc826dfb20733d7e312c27db9c7848b65df5d
miguelcoria94 <miguelcoria94@gmail.com>
2020-11-06 17:48:11 -0600

    added ratings order for table

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,32 @@
 const express = require('express');
-const router = express.Router();
+
+const db = require("../db/models")
 
 const { requireAuth } = require("../auth");
 const { csrfProtection, asyncHandler } = require("./utils"); //need make utils
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', csrfProtection, function(req, res, next) {
-  res.render('index', { title: "Home" , csrfToken: req.csrfToken() });
-});
+router.get('/',
+  csrfProtection,
+  asyncHandler(async (req, res, next) => {
+    let podcasts = await db.Podcast.findAll({
+      order: [["title", "ASC"]],
+      limit: 5,
+    }).map(async (pod) => ({
+      id: pod.id,
+      host: pod.host,
+      description: pod.description,
+      hostName: pod.hostName,
+      title: pod.title,
+      podcastImage: pod.podcastImage,
+      rating: (await db.Rating.findOne({
+        where: { podcastId: pod.id }
+      }))
+    }));
+
+    podcasts.sort((a, b) => parseInt(b.rating.rating, 10) - parseInt(a.rating.rating, 10))
+  res.render('index', { title: "Home" , csrfToken: req.csrfToken(), podcasts });
+}));
 
 module.exports = router;
