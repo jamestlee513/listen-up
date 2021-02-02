c617668ac7660c42db603a3ab40cf088c58394a0
julia-richards <julia.m.richards@gmail.com>
2021-02-02 15:49:42 -0700

    maintains order review after edit, adds config to remove dotenv arg

--- config/index.js
+++ config/index.js
@@ -1,11 +1,13 @@
+require("dotenv").config();
+
 module.exports = {
-    environment: process.env.NODE_ENV || 'development',
-    port: process.env.PORT || 8080,
-    sessionSecret: process.env.SESSION_SECRET,
-    db: {
-        username: process.env.DB_USERNAME,
-        password: process.env.DB_USERNAME,
-        database: process.env.DB_DATABASE,
-        host: process.env.DB_HOST,
-    },
+  environment: process.env.NODE_ENV || "development",
+  port: process.env.PORT || 8080,
+  sessionSecret: process.env.SESSION_SECRET,
+  db: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_USERNAME,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+  },
 };
